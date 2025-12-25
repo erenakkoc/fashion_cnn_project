8a36bd1f219e683308de305ff6a0919dfa39d102
Eren Akkoç <126559002+erenakkoc@users.noreply.github.com>
2025-12-25 17:41:51 +0300

    Update egitim.py

--- egitim.py
+++ egitim.py
@@ -1,21 +1,16 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import matplotlib
-matplotlib.use("TkAgg")  # veya "Agg" sadece kaydetmek için
+matplotlib.use("TkAgg") 
 import matplotlib.pyplot as plt
 
 
-# =========================
-# AYARLAR
-# =========================
 DATASET_DIR = r"C:\Users\Eren\Desktop\fashion-dataset\dataset2"
 IMG_SIZE = (128, 128)
 BATCH_SIZE = 8
 EPOCHS = 15
 
-# =========================
-# DATASET
-# =========================
+
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     DATASET_DIR + "/train",
     image_size=IMG_SIZE,
@@ -35,16 +30,12 @@ class_names = train_ds.class_names
 NUM_CLASSES = len(class_names)
 print("Sınıf sayısı:", NUM_CLASSES)
 
-# =========================
-# NORMALIZATION
-# =========================
+
 normalization = layers.Rescaling(1./255)
 train_ds = train_ds.map(lambda x, y: (normalization(x), y))
 test_ds = test_ds.map(lambda x, y: (normalization(x), y))
 
-# =========================
-# CNN MODEL
-# =========================
+
 model = models.Sequential([
     layers.Conv2D(32, (3,3), activation="relu", input_shape=(128,128,3)),
     layers.MaxPooling2D(2,2),
@@ -69,18 +60,14 @@ model.compile(
 
 model.summary()
 
-# =========================
-# EĞİTİM
-# =========================
+
 history = model.fit(
     train_ds,
     validation_data=test_ds,
     epochs=EPOCHS
 )
 
-# =========================
-# GRAFİKLER
-# =========================
+
 plt.plot(history.history["accuracy"], label="Train Acc")
 plt.plot(history.history["val_accuracy"], label="Val Acc")
 plt.legend()
@@ -93,8 +80,6 @@ plt.legend()
 plt.title("Loss")
 plt.show()
 
-# =========================
-# MODEL KAYDET
-# =========================
+
 model.save("fashion_cnn_30class.h5")
 print("✅ Model kaydedildi: fashion_cnn_30class.h5")
